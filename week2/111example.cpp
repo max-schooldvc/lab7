b0ca56c7944c1275fa5900c74c993f0a7e8bedf5
max-schooldvc <johnburner3133@gmail.com>
2025-09-09 09:51:53 -0700

    2

---
+++ week2/111example.cpp
@@ -0,0 +1,22 @@
+// COMSC-210 | Lab xx | Max Kechely
+// IDE used: VSCode 
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+//have prototypes here
+void prototype();
+
+int main() {
+    // does something
+
+    return 0;
+}
+
+void prototype() {
+    // does something
+}
---
+++ week3/lab7/lab7.cpp
@@ -0,0 +1,13 @@
+// COMSC-210 | Lab 5 | Max Kechely
+// IDE used: VSCode
+
+#include <iostream> 
+using namespace std;
+
+
+
+
+
+int main(){
+
+}
